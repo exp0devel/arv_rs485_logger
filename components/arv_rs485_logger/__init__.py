de85418e0a506b8199f905c724f2d6022cde3c54
exp0devel <umirzakov.nurtas@gmail.com>
2025-11-08 20:47:52 +0500

    modified:   components/arv_rs485_logger/__init__.py

--- components/arv_rs485_logger/__init__.py
+++ components/arv_rs485_logger/__init__.py
@@ -1,17 +1,22 @@
 from esphome import codegen as cg, config_validation as cv
-from esphome.const import CONF_ID
 from esphome.components import uart
+from esphome.const import CONF_ID
 
-DEPENDENCIES = ['uart']
+DEPENDENCIES = ["uart"]
+CODEOWNERS = ["@exp0devel"]
 
-arv_rs485_logger_ns = cg.esphome_ns.namespace('arv_rs485_logger')
-ArvRs485Logger = arv_rs485_logger_ns.class_('ArvRs485Logger', cg.Component, uart.UARTDevice)
+arv_ns = cg.esphome_ns.namespace("arv_rs485_logger")
+ArvRs485Logger = arv_ns.class_("ArvRs485Logger", cg.Component, uart.UARTDevice)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(ArvRs485Logger),
-}).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+CONFIG_SCHEMA = (
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(ArvRs485Logger),
+    })
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(uart.UART_DEVICE_SCHEMA)
+)
 
-async def to_code(config):
+def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
+    yield cg.register_component(var, config)
+    yield uart.register_uart_device(var, config)
